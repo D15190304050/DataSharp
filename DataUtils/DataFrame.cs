9c728d25a8acfd20bb6a4a88904796d9250fe8df
鲍方舟 <shakuganno00@163.com>
2018-08-21 23:53:10 +0800

    Last updated: 2018.08.21 23:53

--- DataUtils/DataFrame.cs
+++ DataUtils/DataFrame.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace DataUtils
 {
+    // Unsolved:
+    // * Implement Linq interfaces such as Where().
+
+
     /// <summary>
     /// The DataFrame class represents a data table that holds structured data. Each row in the table represents a record, and each column in the table represents a kind of attribute.
     /// </summary>
@@ -35,6 +39,22 @@ namespace DataUtils
 
             // Modify the data row in-place, add a new data row if the row label does not exist before.
             set { }
+        }
+
+        /// <summary>
+        /// Gets the names of the row level indices.
+        /// </summary>
+        public IEnumerable<string> RowNames
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Gets the names of the column level indices.
+        /// </summary>
+        public IEnumerable<string> ColumnNames
+        {
+            get { return null; }
         }
 
         /// <summary>
@@ -75,7 +95,7 @@ namespace DataUtils
         }
 
         /// <summary>
-        /// Returns a copy of this <see cref="DataFrame" /> with the specified row removed.
+        /// Returns a deep copy of this <see cref="DataFrame" /> with the specified row removed.
         /// </summary>
         /// <param name="rowIndex">The index of the row to be removed.</param>
         /// <returns>A copy of this <see cref="DataFrame" /> with the specified row removed.</returns>
@@ -85,7 +105,7 @@ namespace DataUtils
         }
 
         /// <summary>
-        /// Returns a copy of this <see cref="DataFrame" /> with the specified row removed.
+        /// Returns a deep copy of this <see cref="DataFrame" /> with the specified row removed.
         /// </summary>
         /// <param name="rowName">The name of the row to be removed.</param>
         /// <returns>A copy of this <see cref="DataFrame" /> with the specified row removed.</returns>
@@ -113,7 +133,7 @@ namespace DataUtils
         }
 
         /// <summary>
-        /// Returns a copy of this <see cref="DataFrame" /> with the specified column removed.
+        /// Returns a deep copy of this <see cref="DataFrame" /> with the specified column removed.
         /// </summary>
         /// <param name="columnIndex">The index of the column to be removed.</param>
         /// <returns>A copy of this <see cref="DataFrame" /> with the specified column removed.</returns>
@@ -123,7 +143,7 @@ namespace DataUtils
         }
 
         /// <summary>
-        /// Returns a copy of this <see cref="DataFrame" /> with the specified column removed.
+        /// Returns a deep copy of this <see cref="DataFrame" /> with the specified column removed.
         /// </summary>
         /// <param name="columnName">The name of the column to be removed.</param>
         /// <returns>A copy of this <see cref="DataFrame" /> with the specified column removed.</returns>
@@ -133,7 +153,8 @@ namespace DataUtils
         }
 
         /// <summary>
-        /// Removes a specified column from this <see cref="DataFrame" /> in-place without creating a new instance of <see cref="DataFrame" />.
+        /// Removes a specified column from this <see cref="DataFrame" /> in-place without creating a new instance of
+        /// <see cref="DataFrame" />.
         /// </summary>
         /// <param name="columnIndex">The index of the column to be removed.</param>
         public void RemoveColumnInPlace(int columnIndex)
@@ -142,7 +163,8 @@ namespace DataUtils
         }
 
         /// <summary>
-        /// Removes a specified column from this <see cref="DataFrame" /> in-place without creating a new instance of <see cref="DataFrame" />.
+        /// Removes a specified column from this <see cref="DataFrame" /> in-place without creating a new instance of
+        /// <see cref="DataFrame" />.
         /// </summary>
         /// <param name="columnName">The name of the column to be removed.</param>
         public void RemoveColumnInPlace(string columnName)
@@ -151,8 +173,8 @@ namespace DataUtils
         }
 
         /// <summary>
-        /// Renames and returns the specified columns in the keys of the given dictionary to the values in the same
-        /// dictionary, respectively.
+        /// Returns a deep copy of this <see cref="DataFrame" /> with the specified columns in the keys of the given
+        /// dictionary changed to the values in the same dictionary, respectively.
         /// </summary>
         /// <param name="columns">The dictionary that contains the names to rename.</param>
         /// <returns>A new <see cref="DataFrame" /> instance with the specified columns renamed.</returns>
@@ -161,7 +183,7 @@ namespace DataUtils
         /// in the same dictionary, respectively. This operation only change the name of the columns, without changing
         /// the data of the columns.
         /// </remarks>
-        public DataFrame Rename(Dictionary<string, string> columns)
+        public DataFrame RenameColumns(Dictionary<string, string> columns)
         {
             return null;
         }
@@ -176,7 +198,47 @@ namespace DataUtils
         /// in the same dictionary, respectively. This operation only change the name of the columns, without changing
         /// the data of the columns.
         /// </remarks>
-        public void RenameInPlace(Dictionary<string, string> columns)
+        public void RenameColumnsInPlace(Dictionary<string, string> columns)
+        {
+        }
+
+        /// <summary>
+        /// Returns a deep copy of this <see cref="DataFrame" /> with the specified rows in the keys of the given
+        /// dictionary changed to the values in the same dictionary, respectively.
+        /// </summary>
+        /// <param name="rows">The dictionary that contains the names to rename.</param>
+        /// <returns>A new <see cref="DataFrame" /> instance with the specified rows renamed.</returns>
+        /// <remarks>
+        /// The original names of rows are the keys of the given dictionary, the new names of rows are the values
+        /// in the same dictionary, respectively. This operation only change the name of the rows, without changing
+        /// the data of the rows.
+        /// </remarks>
+        public DataFrame RenameRows(Dictionary<string, string> rows)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Renames the specified rows in the keys of the given dictionary to the values in the same dictionary,
+        /// respectively, in-place.
+        /// </summary>
+        /// <param name="rows">The dictionary that contains the names to rename.</param>
+        /// <remarks>
+        /// The original names of rows are the keys of the given dictionary, the new names of rows are the values
+        /// in the same dictionary, respectively. This operation only change the name of the rows, without changing
+        /// the data of the rows.
+        /// </remarks>
+        public void RenameRowsInPlace(Dictionary<string, string> rows)
+        {
+        }
+
+        // Maybe this method would be removed.
+        /// <summary>
+        /// Sets the <see cref="DataFrame" /> row names using one existing columns.
+        /// </summary>
+        /// <param name="columnName"></param>
+        [Obsolete]
+        public void SetRowNames(string columnName)
         {
         }
 
