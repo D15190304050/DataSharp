8234243c4ab5d159b166091cec16ab4d7fd67eab
鲍方舟 <shakuganno00@163.com>
2018-10-07 19:17:55 +0800

    Last updated: 2018.10.07 19:17

--- Mathematics/Tensor.cs
+++ Mathematics/Tensor.cs
@@ -29,5 +29,38 @@ namespace Mathematics
         {
             return null;
         }
+
+        /// <summary>
+        /// Returns a boolean mask where the entry equals 1 or 0.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static Tensor<T> operator >(Tensor<T> left, Tensor<T> right)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Returns a boolean mask where the entry equals 1 or 0.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static Tensor<T> operator <(Tensor<T> left, Tensor<T> right)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="axis">Sort along which axis.</param>
+        /// <param name="inPlace">Sorting in-place or not.</param>
+        /// <returns></returns>
+        public Tensor<T> Sort(int axis = 0, bool inPlace = true)
+        {
+            return null;
+        }
     }
 }
