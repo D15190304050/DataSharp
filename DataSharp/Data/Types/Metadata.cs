1d4c08a8f0aa291dc307b5fce88b137c7089cb48
DinoStark <15190304050@163.com>
2022-01-04 01:11:07 +0800

    Keep coding...

--- DataSharp/Data/Types/Metadata.cs
+++ DataSharp/Data/Types/Metadata.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataSharp.Mathematics;
 
 namespace DataSharp.Data.Types
 {
@@ -9,11 +10,33 @@ namespace DataSharp.Data.Types
     {
         private Dictionary<string, object> dictionary;
 
+        public static Metadata Empty { get; }
+
+        static Metadata()
+        {
+            Empty = new Metadata(new Dictionary<string, object>());
+        }
+
+        public Metadata(Dictionary<string, object> dictionary)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            this.dictionary = dictionary;
+        }
+
         public bool ContainsKey(string key)
         {
             return dictionary.ContainsKey(key);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public bool TryGetValue<T>(string key, out T value)
         {
             object o = dictionary[key];
@@ -30,10 +53,46 @@ namespace DataSharp.Data.Types
         {
             if (o != null && o is Metadata other)
             {
-                
+                Dictionary<string, object> thisDictionary = this.dictionary;
+                Dictionary<string, object> otherDictionary = other.dictionary;
+
+                if (thisDictionary.Count != otherDictionary.Count)
+                    return false;
+
+                foreach (string key in thisDictionary.Keys)
+                {
+                    if (!otherDictionary.ContainsKey(key))
+                        return false;
+
+                    object thisValue = thisDictionary[key];
+                    object otherValue = otherDictionary[key];
+
+                    if (thisValue == null || otherValue == null)
+                        return false;
+
+                    if (!thisValue.Equals(otherValue))
+                        return false;
+                }
+
+                return true;
             }
 
             return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return dictionary.GetHashCode();
+        }
+
+        /// <summary>
+        /// Computes the hash code for the types we support.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static int GetHashCode(object o)
+        {
+            return 0;
         }
     }
 }
